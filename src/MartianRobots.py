70236a69a80b8bf670e40d7d3124056703a77e02
Javier Rodríguez Sánchez <jrodriguez@bankinter.com>
2019-02-24 17:32:02 +0100

    Added Normal and Verbose mode

--- src/MartianRobots.py
+++ src/MartianRobots.py
@@ -1,8 +1,10 @@
 
 import sys
 import re
+import collections
 
-test = False
+# Can be on of Test, Normal or Verbose
+mode = 'Normal'
 
 orientation = ('N', 'S', 'E', 'W')
 command = ('F', 'L', 'R')
@@ -101,7 +103,7 @@ class Robot:
         return True
 
 
-if test is not True:
+if mode == 'Verbose':
     inputString = raw_input("Please, input the planet size in this format: 23 32 (Max 50x50)\n")
     valid = bool(re.match("^(([0-9]|[0-4][0-9]|50)\s([0-9]|[0-4][0-9]|50))$", inputString))
     if valid != True:
@@ -114,7 +116,7 @@ if test is not True:
 
     print("Planet size: {}x{}\n".format(xSize, ySize))
 
-    listRobots = {}
+    listRobots = collections.OrderedDict()
 
     moreInput = True
     while moreInput:
@@ -162,6 +164,67 @@ if test is not True:
     del listInput
     del tempRobot
     del mars
+elif mode == 'Normal':
+    inputString = raw_input()
+    if not inputString:
+        sys.exit(-1)
+    valid = bool(re.match("^(([0-9]|[0-4][0-9]|50)\s([0-9]|[0-4][0-9]|50))$", inputString))
+    if valid != True:
+        sys.exit(-1)
+
+    listInput = inputString.split(' ')
+    xSize = int(listInput[0])
+    ySize = int(listInput[1])
+
+    listRobots = {}
+
+    moreInput = True
+    while moreInput:
+        inputString = raw_input()
+        if not inputString:
+            break
+        valid = bool(re.match("^(([0-9]|[0-4][0-9]|50)\s([0-9]|[0-4][0-9]|50)\s(W|E|N|S))$", inputString))
+        if valid is not True:
+            del inputString
+            del valid
+            del moreInput
+            sys.exit(-1)
+        listInput = inputString.split(' ')
+        if int(listInput[0]) > xSize or int(listInput[1]) > ySize:
+            del inputString
+            del valid
+            del moreInput
+            del listInput
+            sys.exit(-1)
+        tempRobot = Robot(int(listInput[0]), int(listInput[1]), str(listInput[2]))
+        inputString = raw_input()
+        valid = bool(re.match("^(R|F|L){1,100}$", inputString))
+        if valid is not True:
+            del inputString
+            del valid
+            del moreInput
+            del listInput
+            del tempRobot
+            sys.exit(-1)
+        listRobots.update({tempRobot: inputString})
+        inputString = raw_input()
+        if inputString:
+            inputString = raw_input()
+            if inputString:
+                moreInput = False
+
+    mars = Map(xSize, ySize)
+    for key, value in listRobots.items():
+        for i in value:
+            key.move(i, mars)
+        print("{} {} {} {}".format(key.xPos, key.yPos, key.orientation, 'LOST' if key.isLost else ''))
+    del listRobots
+    del inputString
+    del valid
+    del moreInput
+    del listInput
+    del tempRobot
+    del mars
 else:
     print("---Test Mode---")
     # We initiliaze the planet landscape
@@ -177,7 +240,7 @@ else:
     r1.move('F', mars)
     r1.move('R', mars)
     r1.move('F', mars)
-    print(r1.isLost, r1.xPos, r1.yPos, r1.orientation)
+    print("{} {} {} {}".format(r1.xPos, r1.yPos, r1.orientation, 'LOST' if r1.isLost else ''))
     del r1
 
     r2 = Robot(3, 2, 'N')
@@ -194,7 +257,7 @@ else:
     r2.move('F', mars)
     r2.move('L', mars)
     r2.move('L', mars)
-    print(r2.isLost, r2.xPos, r2.yPos, r2.orientation)
+    print("{} {} {} {}".format(r2.xPos, r2.yPos, r2.orientation, 'LOST' if r2.isLost else ''))
     del r2
 
     r3 = Robot(0, 3, 'W')
@@ -208,7 +271,7 @@ else:
     r3.move('L', mars)
     r3.move('F', mars)
     r3.move('L', mars)
-    print(r3.isLost, r3.xPos, r3.yPos, r3.orientation)
+    print("{} {} {} {}".format(r3.xPos, r3.yPos, r3.orientation, 'LOST' if r3.isLost else ''))
     del r3
 
     del mars
